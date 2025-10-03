f6ab37e7f1e8ed7bbb7395e1e20804130c478e4b
Samuel Morales <samo.m.g.17@gmail.com>
2025-10-03 17:43:39 -0300

    avance

--- main.py
+++ main.py
@@ -3,18 +3,19 @@
 from gestion_parcelas import Parcela 
 
 
-p = Parcela(id_parcela=1, superficie_ha=10.5, cultivo_actual="Trigo")
+p = Parcela(id_parcela=1, superficie_ha=10.5, cultivo_actual="choclo")
 
-p.actualizar_cultivo("Maíz")
+p.actualizar_cultivo("choclo")
 p.rectificar_superficie(12, "Medición más precisa")
 p.desactivar("Descanso de la tierra")
 try:
-    p.actualizar_cultivo("Soja")  # Esto lanzará excepción
+    p.actualizar_cultivo("manzanas")  
 except Exception as e:
     print("Error:", e)
 
 p.activar("Se reanuda la producción")
-p.actualizar_cultivo("Soja")
+p.actualizar_cultivo("manzanas")
 
 p.mostrar_historial()
+
 
